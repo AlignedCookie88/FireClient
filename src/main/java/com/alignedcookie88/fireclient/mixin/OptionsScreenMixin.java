22bc4ee307dac8d0c18592a23f55955045b47926
AlignedCookie88 <52969840+AlignedCookie88@users.noreply.github.com>
2024-07-17 16:26:40 +0100

    move fireclient options button from title screen to options menu

---
+++ src/main/java/com/alignedcookie88/fireclient/mixin/OptionsScreenMixin.java
@@ -0,0 +1,40 @@
+package com.alignedcookie88.fireclient.mixin;
+
+import com.alignedcookie88.fireclient.Config;
+import com.alignedcookie88.fireclient.FireClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.GridWidget;
+import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Supplier;
+
+@Mixin(OptionsScreen.class)
+public abstract class OptionsScreenMixin {
+
+    @Shadow protected abstract ButtonWidget createButton(Text message, Supplier<Screen> screenSupplier);
+
+    @Redirect(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;", ordinal = 11))
+    public <T extends Widget> T init(GridWidget.Adder instance, T widget) {
+
+        T added = instance.add(widget);
+
+        instance.add(
+                this.createButton(
+                        Text.literal("FireClient"),
+                        () -> Config.getConfig((Screen) (Object) this)
+                )
+        );
+
+        return added;
+    }
+
+}
