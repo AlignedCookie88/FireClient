ece03908c50aa08bb98985884ef0e9e446c82b52
AlignedCookie88 <52969840+AlignedCookie88@users.noreply.github.com>
2024-11-18 19:38:41 +0000

    [Feature] Include fireclient:pixelate post-processing shader + [Fix] Fix vanilla bug that stops post-processing shaders in a non-vanilla namespace from properly functioning

---
+++ src/main/java/com/alignedcookie88/fireclient/mixin/JsonEffectShaderProgramMixin.java
@@ -0,0 +1,55 @@
+package com.alignedcookie88.fireclient.mixin;
+
+import com.alignedcookie88.fireclient.FireClient;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gl.JsonEffectShaderProgram;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(JsonEffectShaderProgram.class)
+public class JsonEffectShaderProgramMixin {
+
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;ofVanilla(Ljava/lang/String;)Lnet/minecraft/util/Identifier;", ordinal = 0))
+    public Identifier fixIdentifier(String path, Operation<Identifier> original) {
+
+        if (path.startsWith("shaders/program/"))
+            path = path.replaceFirst("shaders/program/", "");
+
+
+        Identifier tried = Identifier.tryParse(path);
+        if (tried == null)
+            tried = Identifier.of("minecraft", path);
+
+        String path2 = tried.getPath();
+
+        if (path2.startsWith("shaders/program/"))
+            return tried;
+
+        return Identifier.of(tried.getNamespace(), ("shaders/program/"+path2+".json").replace(".json.json", ".json"));
+    }
+
+    @WrapOperation(method = "loadEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;ofVanilla(Ljava/lang/String;)Lnet/minecraft/util/Identifier;", ordinal = 0))
+    private static Identifier fixIdentifier2(String path, Operation<Identifier> original) {
+
+        String[] parts = path.split("\\.");
+        String extension = "." + parts[parts.length - 1];
+
+        if (path.startsWith("shaders/program/"))
+            path = path.replaceFirst("shaders/program/", "");
+
+
+        Identifier tried = Identifier.tryParse(path);
+        if (tried == null)
+            tried = Identifier.of("minecraft", path);
+
+        String path2 = tried.getPath();
+
+        if (path2.startsWith("shaders/program/"))
+            return tried;
+
+        return Identifier.of(tried.getNamespace(), ("shaders/program/"+path2+extension).replace(extension+extension, extension));
+    }
+
+}
